b65ee81efe7213b6138a1a2b477f1d817fac5931
Adam Jones <adam.jones@yourenquirymanager.com.au>
2013-10-21 13:27:42 +1000

    Finish static pages.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ MyDemoApp::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
+  get "static_pages/contact"
   resources :accounts
 
   resources :users
